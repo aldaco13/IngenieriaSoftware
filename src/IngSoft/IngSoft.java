f6dad6421da0bf69dab7022e8f69f39a4591be0a
aldaco13 <omar.aldaco.m@gmail.com>
2022-12-01 23:23:33 -0600

    Mensaje nuevo commit

--- src/IngSoft/IngSoft.java
+++ src/IngSoft/IngSoft.java
@@ -4,7 +4,10 @@ public class IngSoft {
 
 	public static void main(String[] args) {
 		System.out.println("Repositorio git");
-
+		System.out.println("Primer commit OAM");
+		System.out.println("Asesoria ing soft");
+		System.out.println("Rama OAM_Impresion_Mensajes");
+		System.out.println("nuevo commit");
 	}
 
 }
