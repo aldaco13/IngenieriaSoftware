299d68cf8bf85a6a1f5f860eee2476bc7fec9a8e
aldaco13 <omar.aldaco.m@gmail.com>
2022-12-05 22:15:29 -0600

    Commit para provocar conflictos en merge

--- src/IngSoft/IngSoft.java
+++ src/IngSoft/IngSoft.java
@@ -4,8 +4,8 @@ public class IngSoft {
 
 	public static void main(String[] args) {
 		System.out.println("Repositorio git");
-		System.out.println("Primer commit OAM");
-		System.out.println("Asesoria ing soft");
+		System.out.println("Commit para merge");
+		System.out.println("Sesion 2 git");
 		System.out.println("Rama OAM_Impresion_Mensajes");
 		System.out.println("nuevo commit");
 	}
