093b4a6d08e646615c444fcb1d9d75496b42c260
AndrikGiovanni212 <cbi2193054547@izt.uam.mx>
2022-12-05 22:50:32 -0600

    Commit 1

--- src/IngSoft/IngSoft.java
+++ src/IngSoft/IngSoft.java
@@ -5,7 +5,7 @@ public class IngSoft {
 	public static void main(String[] args) {
 		System.out.println("Repositorio git");
 		System.out.println("Primer commit BGG");
-		System.out.println("Ayudame, no sé aguardar archivos. Jajaja");
+		System.out.println("Ayudame, no sé aguardar archivos. Jajaja ");
 		System.out.println("Rama OAM_Impresion_Mensajes");
 		
 	}
